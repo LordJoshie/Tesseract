xpocketmc/XPocketMP-server

A bunch of really dirty hacks to get 1.1.0.0 working Mojang: once again, what the hell have you done.

src/pocketmine/network/mcpe/protocol/EntityFallPacket.php
@@ -26,8 +26,8 @@ namespace pocketmine\network\mcpe\protocol;
 
 use pocketmine\network\mcpe\NetworkSession;
 
-class PlayerFallPacket extends DataPacket{
-	const NETWORK_ID = ProtocolInfo::PLAYER_FALL_PACKET;
+class EntityFallPacket extends DataPacket{
+	const NETWORK_ID = ProtocolInfo::ENTITY_FALL_PACKET;
 
 	public $fallDistance;
 
@@ -40,6 +40,6 @@ class PlayerFallPacket extends DataPacket{
 	}
 
 	public function handle(NetworkSession $session) : bool{
-		return $session->handlePlayerFall($this);
+		return $session->handleEntityFall($this);
 	}
 }

LordJoshie/Tesseract

src/pocketmine/network/protocol/EntityFallPacket.php
@@ -24,9 +24,9 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
-class PlayerFallPacket extends DataPacket{
+class EntityFallPacket extends DataPacket{
 
-	const NETWORK_ID = Info::PLAYER_FALL_PACKET;
+	const NETWORK_ID = Info::ENTITY_FALL_PACKET;
 
 	public $fallDistance;
 